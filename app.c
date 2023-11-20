c3bae69642056bd4d72aff0df9cc3064ee0c3414
mohamed-ameziane <med.ameez02@gmail.com>
2023-11-20 09:10:34 +0100

    Manipulation des images RGB en C.

---
+++ app.c
@@ -0,0 +1,22 @@
+/*
+Manipulation d’images RGB par les opérations suivantes :
+    1. Création d’une image sachant ses caractéristiques (longueur L, Hauteur H et sa Résolution R)
+    2. Initialisation des données de l’image
+        a. Par des valeurs aléatoires
+        b. Saisie sur le clavier
+        c. Depuis un fichier
+    3. Traitement d’image :
+        a. Extraire de l'image une de ces composantes de base : rouge, vert et bleu. Cela crée une image en 
+        niveaux de gris,
+        b. Appliquer des filtres pour effectuer des opérations telles que le flou, la détection de contours, 
+        ou la suppression du bruit
+*/
+
+#include <stdio.h>
+#include "functions.h"
+
+
+int main() {
+
+    return 0;
+}
